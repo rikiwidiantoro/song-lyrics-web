e5a33a60c28128ac2862eafc405af35ea4d0a4a3
Riki Widiantoro <rikitoro22@gmail.com>
2023-06-19 21:32:45 +0700

    add react bootstrap and edit home

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Root from "./containers/Root/Root.container";
 import Home from "./containers/Home/Home.container";
 import NotFound from "./component/NotFound/NotFound.component";
 import AllSong from "./containers/AllSong/AllSong.container";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const router = createBrowserRouter(
--- src/containers/Home/Home.container.js
+++ src/containers/Home/Home.container.js
@@ -1,6 +1,28 @@
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 function Home() {
   return (
-    <h1>Home</h1>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Home</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Song Lyrics Web App merupakan website yang menyediakan berbagai lirik lagu dari berbagai music di dunia.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant="secondary"><Link>All Song</Link></Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
